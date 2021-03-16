ef1e56406df4e039c7aca664a773999701104bb8
Rynay <feirs911@gmail.com>
2021-03-16 23:01:06 +0300

    header styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,29 @@
 import s from './Header.module.css'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
   return ( 
-    <div>Header</div>
+    <header className={s.header}>
+      <NavLink to="/">
+        <div className={s.logo}>
+          <img src="img/logo.svg" alt="logo"/>
+        </div>
+      </NavLink>
+      <nav className={s.nav}>
+        <ul>
+          <li><NavLink to="/products" activeClassName={s.active}>Products</NavLink></li>
+          <li><NavLink to="/" activeClassName={s.active}>Mac</NavLink></li>
+          <li><NavLink to="/" activeClassName={s.active}>iPad</NavLink></li>
+          <li><NavLink to="/" activeClassName={s.active}>iPhone</NavLink></li>
+          <li><NavLink to="/" activeClassName={s.active}>Watch</NavLink></li>
+          <li className={s.cart}>
+            <NavLink to="/cart" activeClassName={s.active}>
+              <img src="img/bag.svg" alt="Cart"/>
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
    );
 }
  
