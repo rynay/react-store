4c3ebb13dd5794816bb833de82ead8059e0ae341
Rynay <feirs911@gmail.com>
2021-03-17 01:02:16 +0300

    starter title

--- src/components/Pages/Starter/Starter.js
+++ src/components/Pages/Starter/Starter.js
@@ -3,7 +3,9 @@ import s from './Starter.module.css';
 const Starter = () => {
   return ( 
     <section className={s.starter}>
-      <h2 className={s.slogan}>Think Different</h2>
+      <h2 className={s.slogan}>
+        Think Different
+      </h2>
     </section>
    );
 }
