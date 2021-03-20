93df6a8f63aff9c23ec2dd944401f40925ccf84e
Rynay <feirs911@gmail.com>
2021-03-20 10:00:08 +0300

    incrementing and decrementing items in cart, displaying total price

--- src/components/Pages/Cart/Cart.js
+++ src/components/Pages/Cart/Cart.js
@@ -5,11 +5,15 @@ import s from './Cart.module.css';
 const Cart = () => {
   const cart = useSelector(store => store.cart);
 
+  const total = cart.reduce((acc, item) => acc + item.total, 0);
+
   const items = cart.map(item => <CartProduct key={item.id} item={item}/>)
 
   return ( 
-    <section>
+    <section className={s.container}>
       { items }
+
+      {total ? <div className={s.total}>Total: $ {total}</div> : null}
     </section>
    );
 }
--- src/components/Pages/Cart/CartProduct/index.js
+++ src/components/Pages/Cart/CartProduct/index.js
@@ -1,16 +1,32 @@
+import s from './CartProduct.module.css';
+import { useDispatch } from 'react-redux';
+import { incrementItemAction, decrementItemAction } from '../../../../store/cartReducer'
+
 const CartProduct = ({ item }) => {
   const { title, img, count, total } = item;
-  console.log(item)
+  const dispatch = useDispatch();
+
+  const incItem = () => {
+    dispatch(incrementItemAction(item.id));
+  }
+
+  const decItem = () => {
+    dispatch(decrementItemAction(item.id));
+  }
 
   return (
-    <section>
-      <h2>{ title }</h2>
-      <div>
+    <section className={s.item}>
+      <div className={s.image_container}>
         <img src={img} alt=""/>
       </div>
-      <div>
-        <span>{ count }</span>
-        <span>{ total }</span>
+      <h2 className={s.title}>{ title }</h2>
+      <div className={s.info}>
+        <div className={s.count}>
+          <button onClick={incItem} className={s.button}> + </button> 
+          <div className={s.counter}>{ count }</div> 
+          <button onClick={decItem} className={s.button}> - </button>
+        </div>
+        <div className={s.total}> $ { total }</div>
       </div>
     </section>
   )
