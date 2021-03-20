8df817e8962a9678f9f43275d72b693e62930ae8
Rynay <feirs911@gmail.com>
2021-03-20 09:01:59 +0300

    conditional button if item is in cart or not

--- src/components/Pages/Products/Product/Product.js
+++ src/components/Pages/Products/Product/Product.js
@@ -1,13 +1,17 @@
+import { useSelector } from 'react-redux';
 import s from './Product.module.css'
 
 const Product = ({ productInfo }) => {
-  const { title, img, price, inCart } = productInfo;
+  const { title, img, price, id } = productInfo;
+  const cart = useSelector(store => store.cart);
+
+  const isInCart = cart.includes(prod => prod.id === id);
 
   return ( 
     <section className={s.product}>
       <div className={s.product__image_container}>
         <img src={img} alt=""/>
-        <button className={s.button} disabled={inCart}>{inCart ? 'In Cart' : 'Add'}</button>
+        <button className={s.button} disabled={isInCart}>{isInCart ? 'In Cart' : 'Add'}</button>
       </div>
       <div className={s.product__info}>
         <h2 className={s.product__title}>{ title }</h2>
