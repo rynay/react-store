f0809fe96063feca5567d84fee1bec5d4c7e94cb
Rynay <feirs911@gmail.com>
2021-03-24 19:46:02 +0300

    file structure for ProductPage

--- src/components/Pages/Products/ProductPage/index.js
+++ src/components/Pages/Products/ProductPage/index.js
@@ -1,12 +1,3 @@
-import Product from '../Product';
-import s from './ProductPage.module.css';
-
-const ProductPage = () => {
-  return (
-    <section className={s.container}>
-      <Product info />
-    </section>
-  );
-};
+import ProductPage from './ProductPage';
 
 export default ProductPage;
