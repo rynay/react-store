5c7cc0a217210f6eb53fac0179d712a0a1779fbe
Rynay <feirs911@gmail.com>
2021-03-29 11:35:50 +0300

    Product info page + moving to cart if user click In Cart button

--- src/components/Pages/Products/Product/Product.js
+++ src/components/Pages/Products/Product/Product.js
@@ -1,34 +1,66 @@
 import s from './Product.module.css';
+import sInfo from './ProductInfo.module.css';
 import { Link } from 'react-router-dom';
 
 const Product = ({ product, cart, addToCart, info = false }) => {
+  const styles = info ? sInfo : s;
   const { title, img, price, id } = product;
 
   const isInCart = !!cart.filter((prod) => prod.id === id).length;
 
+  const buttons = (
+    <>
+      {info ? (
+        <Link
+          to={'/products'}
+          className={`${styles.button} ${styles.button_back}`}
+        >
+          To Products
+        </Link>
+      ) : null}
+      <Link
+        to={isInCart ? '/cart' : ''}
+        onClick={(e) => {
+          if (isInCart) return;
+          e.preventDefault();
+          addToCart();
+        }}
+        className={`${styles.button} ${styles.button_cart}`}
+        disabled={isInCart}
+      >
+        {isInCart ? 'In Cart' : 'Add'}
+      </Link>
+    </>
+  );
+
+  const productDetails = (
+    <section className={styles.product}>
+      <div className={styles.product__image_container}>
+        <img src={`/${img}`} alt="" />
+        {info ? (
+          <div className={styles.buttons_container}>{buttons}</div>
+        ) : (
+          buttons
+        )}
+      </div>
+      <div className={styles.product__info}>
+        <h2 className={styles.product__title}>{title}</h2>
+        <p className={styles.product__price}>$ {price}</p>
+        {info && <p className={styles.description}>{product.info}</p>}
+      </div>
+    </section>
+  );
+
   return (
-    <Link to={`/product/${product.id}`} className={s.link}>
-      <section className={s.product}>
-        <div className={s.product__image_container}>
-          <img src={`/${img}`} alt="" />
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              addToCart();
-            }}
-            className={s.button}
-            disabled={isInCart}
-          >
-            {isInCart ? 'In Cart' : 'Add'}
-          </button>
-        </div>
-        <div className={s.product__info}>
-          <h2 className={s.product__title}>{title}</h2>
-          <p className={s.product__price}>$ {price}</p>
-        </div>
-        {info && <p className={s.description}>{product.info}</p>}
-      </section>
-    </Link>
+    <>
+      {info ? (
+        productDetails
+      ) : (
+        <Link to={`/product/${product.id}`} className={styles.link}>
+          {productDetails}
+        </Link>
+      )}
+    </>
   );
 };
 
