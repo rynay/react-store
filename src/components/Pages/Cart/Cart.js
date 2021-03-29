af93b48883f968749ed910504e81fce84f0c3b97
Rynay <feirs911@gmail.com>
2021-03-29 15:35:50 +0300

    empty cart banner

--- src/components/Pages/Cart/Cart.js
+++ src/components/Pages/Cart/Cart.js
@@ -1,5 +1,6 @@
 import CartProduct from './CartProduct';
 import s from './Cart.module.css';
+import { Link } from 'react-router-dom';
 
 const Cart = ({ cart, removeAll }) => {
   const total = cart.reduce((acc, item) => acc + item.total, 0);
@@ -7,31 +8,39 @@ const Cart = ({ cart, removeAll }) => {
   const items = cart.map((item) => <CartProduct key={item.id} item={item} />);
 
   return (
-    <div className={s.container}>
-      <section className={s.items_container}>
-        {items.length ? (
-          <>
+    <>
+      {items.length ? (
+        <div className={s.container}>
+          <section className={s.items_container}>
             <p className={s.category}>products</p>
             <p className={s.category}>name of product</p>
             <p className={s.category}>price</p>
             <p className={s.category}>quantity</p>
             <p className={s.category}>remove</p>
             <p className={s.category}>total</p>
-          </>
-        ) : null}
-        {items}
-      </section>
-      {total ? (
-        <div className={s.total}>
-          <button onClick={removeAll} className={s.remove_all}>
-            Remove All
-          </button>
-          <div>Subtotal: $ {total}</div>
-          <div>Taxes: $ {(total * 0.07).toFixed(0)}</div>
-          <div>Total: $ {(total * 1.07).toFixed(0)}</div>
+            {items}
+          </section>
+          <div className={s.total}>
+            <button onClick={removeAll} className={s.remove_all}>
+              Remove All
+            </button>
+            <div>Subtotal: $ {total}</div>
+            <div>Taxes: $ {(total * 0.07).toFixed(0)}</div>
+            <div>Total: $ {(total * 1.07).toFixed(0)}</div>
+          </div>
         </div>
       ) : null}
-    </div>
+      <>
+        {!items.length ? (
+          <div className={s.banner}>
+            <h2>Your cart is empty</h2>
+            <Link to="/products" className={s.shop}>
+              Start Shopping
+            </Link>
+          </div>
+        ) : null}
+      </>
+    </>
   );
 };
 
