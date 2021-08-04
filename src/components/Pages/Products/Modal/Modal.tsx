b53bd0c643c73efaec85fc3dc45cc1f154d1bdbe
Rynay <feirs911@gmail.com>
2021-08-04 07:25:25 +0300

    components/Pages/Products/Modal -> typescript

---
+++ src/components/Pages/Products/Modal/Modal.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { TItem } from '../../../../types'
+import s from './Modal.module.css'
+
+type PropTypes = {
+  item: TItem
+  isOpen: boolean
+  message: string
+}
+
+const Modal = ({ item, isOpen, message }: PropTypes) => {
+  const classes = isOpen ? `${s.title} ${s.open}` : s.title
+  return (
+    <div className={classes}>
+      <p>
+        {item?.title || 'Item'} {message || 'was added to the cart'}
+      </p>
+    </div>
+  )
+}
+
+export default Modal
