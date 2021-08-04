b0ef18fca9a0cce62fb9ff98fa778da75f836715
Rynay <feirs911@gmail.com>
2021-08-04 08:41:44 +0300

    Project => typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import './App.css';
-import Header from './components/Header';
-import Cart from './components/Pages/Cart';
-import Products from './components/Pages/Products';
-import ProductPage from './components/Pages/Products/ProductPage';
-import Banner from './components/Banner';
-import NotFound from './components/NotFound';
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import './App.css'
+import Header from './components/Header'
+import Cart from './components/Pages/Cart'
+import Products from './components/Pages/Products'
+import ProductPage from './components/Pages/Products/ProductPage'
+import Banner from './components/Banner'
+import NotFound from './components/NotFound'
 
-import { store } from './store';
-import { Provider } from 'react-redux';
+import { store } from './store'
+import { Provider } from 'react-redux'
 
 function App() {
   return (
@@ -33,7 +33,7 @@ function App() {
         </Route>
       </Switch>
     </Provider>
-  );
+  )
 }
 
-export default App;
+export default App
