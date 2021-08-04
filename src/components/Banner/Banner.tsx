bc1332b691bb41acb6fbc4adab728d22628c15b7
Rynay <feirs911@gmail.com>
2021-08-04 07:13:21 +0300

    components/Banner -> typescript

---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import s from './Banner.module.css'
+import { Link } from 'react-router-dom'
+
+type PropTypes = {
+  title: string
+}
+
+const Banner = ({ title }: PropTypes) => {
+  return (
+    <div className={s.banner}>
+      <h2>{title}</h2>
+      <Link to="/products" className={s.shop}>
+        Start Shopping
+      </Link>
+    </div>
+  )
+}
+
+export default Banner
